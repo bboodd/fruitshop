4365d3ef042d0f83051b797d876825fe3f6c98bf
bboodd <gkrgus9@gmail.com>
2023-12-12 22:12:58 +0900

    feat: 주문테이블 등록, 주문상품테이블 등록, 상품재고 차감 mapper 생성

--- src/main/java/com/shop/fruitshop/order/OrderMapper.java
+++ src/main/java/com/shop/fruitshop/order/OrderMapper.java
@@ -1,9 +1,24 @@
 package com.shop.fruitshop.order;
 
+import com.shop.fruitshop.domain.Product;
+import com.shop.fruitshop.user.CartDto;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface OrderMapper {
 
     OrderPageProductDto getProductByProductId(Long productId);
+
+    //주문 상품 정보
+    OrderProductDto getOrderProductByProductId(Long productId);
+
+    //주문 테이블 등록
+    int addOrder(OrderDto orderDto);
+
+    //주문 상품 테이블 등록
+    int addOrderProduct(OrderProductDto orderProductDto);
+
+    //상품 재고 차감
+    int reduceStock(Product product);
+
 }
